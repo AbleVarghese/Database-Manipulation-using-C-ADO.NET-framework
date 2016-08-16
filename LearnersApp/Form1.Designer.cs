7fcba0f287d083511659d28a492af95ba85b77ec
AbleVarghese <able.varghese12@gmail.com>
2016-08-16 19:40:16 -0400

    Add project files.

---
+++ LearnersApp/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace StudentInfoDbFirst
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lstDisplay = new System.Windows.Forms.ListBox();
+            this.btnDisplay = new System.Windows.Forms.Button();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.learnerNameTextbox = new System.Windows.Forms.TextBox();
+            this.learnerIdTextbox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.coursesCompletedTextbox = new System.Windows.Forms.TextBox();
+            this.coursesRemainingTextbox = new System.Windows.Forms.TextBox();
+            this.favoriteTeacherTextbox = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label20 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lstDisplay
+            // 
+            this.lstDisplay.FormattingEnabled = true;
+            this.lstDisplay.ItemHeight = 17;
+            this.lstDisplay.Location = new System.Drawing.Point(496, 117);
+            this.lstDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.lstDisplay.Name = "lstDisplay";
+            this.lstDisplay.Size = new System.Drawing.Size(629, 378);
+            this.lstDisplay.TabIndex = 12;
+            // 
+            // btnDisplay
+            // 
+            this.btnDisplay.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDisplay.Location = new System.Drawing.Point(496, 68);
+            this.btnDisplay.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDisplay.Name = "btnDisplay";
+            this.btnDisplay.Size = new System.Drawing.Size(99, 27);
+            this.btnDisplay.TabIndex = 11;
+            this.btnDisplay.Text = "Display All";
+            this.btnDisplay.UseVisualStyleBackColor = true;
+            this.btnDisplay.Click += new System.EventHandler(this.btnDisplay_Click);
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(246, 117);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(2);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(98, 27);
+            this.btnSearch.TabIndex = 25;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnDelete.Location = new System.Drawing.Point(369, 501);
+            this.btnDelete.Margin = new System.Windows.Forms.Padding(2);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(98, 27);
+            this.btnDelete.TabIndex = 24;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnUpdate.Location = new System.Drawing.Point(246, 501);
+            this.btnUpdate.Margin = new System.Windows.Forms.Padding(2);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(98, 27);
+            this.btnUpdate.TabIndex = 23;
+            this.btnUpdate.Text = "Update";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnAdd.Location = new System.Drawing.Point(114, 501);
+            this.btnAdd.Margin = new System.Windows.Forms.Padding(2);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(98, 27);
+            this.btnAdd.TabIndex = 22;
+            this.btnAdd.Text = "Add Customer";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // learnerNameTextbox
+            // 
+            this.learnerNameTextbox.Location = new System.Drawing.Point(214, 239);
+            this.learnerNameTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.learnerNameTextbox.Name = "learnerNameTextbox";
+            this.learnerNameTextbox.Size = new System.Drawing.Size(157, 25);
+            this.learnerNameTextbox.TabIndex = 19;
+            // 
+            // learnerIdTextbox
+            // 
+            this.learnerIdTextbox.Location = new System.Drawing.Point(214, 68);
+            this.learnerIdTextbox.Margin = new System.Windows.Forms.Padding(2);
+            this.learnerIdTextbox.Name = "learnerIdTextbox";
+            this.learnerIdTextbox.Size = new System.Drawing.Size(157, 25);
+            this.learnerIdTextbox.TabIndex = 18;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft MHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(94, 239);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 20);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Learner Name";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft MHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(107, 68);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 20);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Learner ID :";
+            // 
+            // coursesCompletedTextbox
+            // 
+            this.coursesCompletedTextbox.Location = new System.Drawing.Point(210, 443);
+            this.coursesCompletedTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.coursesCompletedTextbox.Name = "coursesCompletedTextbox";
+            this.coursesCompletedTextbox.Size = new System.Drawing.Size(159, 25);
+            this.coursesCompletedTextbox.TabIndex = 36;
+            // 
+            // coursesRemainingTextbox
+            // 
+            this.coursesRemainingTextbox.Location = new System.Drawing.Point(210, 375);
+            this.coursesRemainingTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.coursesRemainingTextbox.Name = "coursesRemainingTextbox";
+            this.coursesRemainingTextbox.Size = new System.Drawing.Size(159, 25);
+            this.coursesRemainingTextbox.TabIndex = 37;
+            // 
+            // favoriteTeacherTextbox
+            // 
+            this.favoriteTeacherTextbox.Location = new System.Drawing.Point(210, 307);
+            this.favoriteTeacherTextbox.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.favoriteTeacherTextbox.Name = "favoriteTeacherTextbox";
+            this.favoriteTeacherTextbox.Size = new System.Drawing.Size(159, 25);
+            this.favoriteTeacherTextbox.TabIndex = 38;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft MHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(58, 443);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(140, 20);
+            this.label5.TabIndex = 33;
+            this.label5.Text = "Courses Completed";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft MHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(60, 375);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(139, 20);
+            this.label6.TabIndex = 34;
+            this.label6.Text = "Courses Remaining";
+            // 
+            // label20
+            // 
+            this.label20.AutoSize = true;
+            this.label20.Font = new System.Drawing.Font("Microsoft MHei", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label20.Location = new System.Drawing.Point(76, 307);
+            this.label20.Name = "label20";
+            this.label20.Size = new System.Drawing.Size(123, 20);
+            this.label20.TabIndex = 35;
+            this.label20.Text = "Favorite Teacher";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1169, 603);
+            this.Controls.Add(this.coursesCompletedTextbox);
+            this.Controls.Add(this.coursesRemainingTextbox);
+            this.Controls.Add(this.favoriteTeacherTextbox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label20);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.learnerNameTextbox);
+            this.Controls.Add(this.learnerIdTextbox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lstDisplay);
+            this.Controls.Add(this.btnDisplay);
+            this.Font = new System.Drawing.Font("Microsoft MHei", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "Form1";
+            this.Text = "Learner Information App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstDisplay;
+        private System.Windows.Forms.Button btnDisplay;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox learnerNameTextbox;
+        private System.Windows.Forms.TextBox learnerIdTextbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox coursesCompletedTextbox;
+        private System.Windows.Forms.TextBox coursesRemainingTextbox;
+        private System.Windows.Forms.TextBox favoriteTeacherTextbox;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label20;
+    }
+}
+
---
+++ LearnersApp/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.Sql;
+using System.Data.SqlClient;
+namespace StudentInfoDbFirst
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnDisplay_Click(object sender, EventArgs e)
+        {
+
+
+            using (var dbContext = new AbleDBEntities())
+            {
+                var query = from a in dbContext.AbleTBs
+                            orderby a.learnerName
+                            select new
+                            {
+                                learnerIdVar = a.learnerId,
+                                learnerNameVar = a.learnerName,
+                                favoriteTeacherVar = a.favoriteTeacher,
+                                coursesRemainingVar = a.coursesRemaining,
+                                coursesCompletedVar = a.coursesCompleted,
+                            };
+
+
+
+
+                lstDisplay.Items.Clear(); //clear the list
+         
+                foreach (var item in query)
+                {
+                  
+                    lstDisplay.Items.Add(item.learnerIdVar + "      " + item.learnerNameVar
+                      + "      " + item.favoriteTeacherVar + "      " + item.favoriteTeacherVar
+                      + "      " + item.coursesRemainingVar + "      " + item.coursesCompletedVar);
+                }
+            }
+        }
+    
+
+    //Search from the database -- Fully Working 
+    private void btnSearch_Click(object sender, EventArgs e)
+    {
+        using (var dbContext = new AbleDBEntities())
+        {
+            int xyz = Convert.ToInt32(learnerIdTextbox.Text);
+            var query = from a in dbContext.AbleTBs
+
+                        where a.learnerId == xyz
+                        select new
+                        {
+
+                            learnerIdVar = a.learnerId,
+                            learnerNameVar = a.learnerName,
+                            favoriteTeacherVar = a.favoriteTeacher,
+                            coursesRemainingVar = a.coursesRemaining,
+                            coursesCompletedVar = a.coursesCompleted,
+
+
+
+                        };
+
+
+            foreach (var item in query)
+            {
+                learnerIdTextbox.Text = item.learnerIdVar.ToString();
+                learnerNameTextbox.Text = item.learnerNameVar;
+                favoriteTeacherTextbox.Text = item.favoriteTeacherVar;
+                coursesRemainingTextbox.Text = item.coursesRemainingVar.ToString();
+                coursesCompletedTextbox.Text = item.coursesCompletedVar.ToString();
+            }
+        }
+    }
+
+
+
+
+    //Add item to the database -- fully working
+    private void btnAdd_Click(object sender, EventArgs e)
+    {
+        using (var dbContext = new AbleDBEntities())
+        {
+            //create an instance of the entity object
+            AbleTB student = new AbleTB
+            {
+                learnerId = Convert.ToInt32(learnerIdTextbox.Text),
+                learnerName = learnerNameTextbox.Text,
+                favoriteTeacher = favoriteTeacherTextbox.Text,
+                coursesRemaining = Convert.ToInt32(coursesRemainingTextbox.Text),
+                coursesCompleted = Convert.ToInt32(coursesCompletedTextbox.Text),
+
+
+            };
+            //add the entity to the data context
+            dbContext.AbleTBs.Add(student);
+            dbContext.AbleTBs.Attach(student);
+            dbContext.Entry(student).State = EntityState.Added;
+            dbContext.SaveChanges();          
+            MessageBox.Show("Saved");
+        }
+    }
+
+    private void btnUpdate_Click(object sender, EventArgs e)
+    {
+        AbleTB student;
+
+        //Get customer for the given customerId
+        using (var dbContext = new AbleDBEntities())
+        {
+            int xyz = Convert.ToInt32(learnerIdTextbox.Text);
+            student = dbContext.AbleTBs.Where(s => s.learnerId == xyz).FirstOrDefault<AbleTB>();
+        }
+
+        // Update customer info in disconnected mode (out of dbContext scope)
+        if (student != null)
+        {
+            student.learnerName = learnerNameTextbox.Text;
+            //  student.lastName = tbLastName.Text;
+        }
+
+        // Save modified entity using new Context
+        using (var dbContext = new AbleDBEntities())
+        {
+            // Mark entity as modified
+            dbContext.Entry(student).State = EntityState.Modified;
+
+            // Save changes to database
+            dbContext.SaveChanges();
+        }
+        MessageBox.Show("Updated");
+    }
+
+
+
+    private void btnDelete_Click(object sender, EventArgs e)
+    {
+        AbleTB studentToDelete;
+        //Get student for the given studentId
+        using (var dbContext = new AbleDBEntities())
+        {
+            int xyz = Convert.ToInt32(learnerIdTextbox.Text);
+            studentToDelete = dbContext.AbleTBs.Where(s => s.learnerId == xyz).FirstOrDefault<AbleTB>();
+        }
+
+        //Create new context for disconnected scenario
+        using (var newContext = new AbleDBEntities())
+        {
+            newContext.Entry(studentToDelete).State = EntityState.Deleted;
+
+            newContext.SaveChanges();
+        }
+    }
+
+
+}
+}
